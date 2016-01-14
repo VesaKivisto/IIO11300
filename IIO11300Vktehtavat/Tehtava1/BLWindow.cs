384b996948eed8b4418b03c7ee9ad2e48041c3ad
VesaKivisto <kivisto.vesku@gmail.com>
2016-01-14 10:46:42 +0200

    Add a business class

---
+++ IIO11300Vktehtavat/Tehtava1/BLWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava1
+{
+    public class BusinessLogicWindow
+    {
+        /// <summary>
+        /// CalculatePerimeter calculates the perimeter of a window
+        /// </summary>
+        public static double CalculatePerimeter(double widht, double height)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
