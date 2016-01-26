64cdfd79b427592fafb89a5bf6bab23784c29cb0
Vesa Kivistö <kivisto.vesku@gmail.com>
2016-01-26 19:00:54 +0200

    Tee tehtävä 2

---
+++ IIO11300Vktehtavat/Tehtava2/BLLotto.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava2
+{
+    class Lotto
+    {
+        #region Muuttujat (variables)
+        Random random = new Random();
+        private int[] numbers;
+        #endregion
+        #region Metodit (methods)
+        public int[] DrawLotto()
+        {
+            // Normiloton arvonta
+            numbers = new int[7];
+
+            for (int i = 0; i < 7; i++)
+            {
+                int nextNumber;
+
+                do
+                {
+                    nextNumber = random.Next(1, 40);
+                } while (numbers.Contains(nextNumber));
+
+                numbers[i] = nextNumber;
+            }
+
+            Array.Sort(numbers);
+            return numbers;
+        }
+        public int[] DrawVikingLotto()
+        {
+            // Viking Loton arvonta
+            numbers = new int[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                int nextNumber;
+
+                do
+                {
+                    nextNumber = random.Next(1, 49);
+                } while (numbers.Contains(nextNumber));
+
+                numbers[i] = nextNumber;
+            }
+
+            Array.Sort(numbers);
+            return numbers;
+        }
+        public int[] DrawEurojackpotMain()
+        {
+            // Eurojackpotin arvonta
+            numbers = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                int nextNumber;
+
+                do
+                {
+                    nextNumber = random.Next(1, 51);
+                } while (numbers.Contains(nextNumber));
+
+                numbers[i] = nextNumber;
+            }
+
+            Array.Sort(numbers);
+            return numbers;
+        }
+
+        public int[] DrawEurojackpotStar()
+        {
+            // Eurojackpotin arvonta
+            numbers = new int[2];
+
+            for (int i = 0; i < 2; i++)
+            {
+                int nextNumber;
+
+                do
+                {
+                    nextNumber = random.Next(1, 9);
+                } while (numbers.Contains(nextNumber));
+
+                numbers[i] = nextNumber;
+            }
+
+            Array.Sort(numbers);
+            return numbers;
+        }
+        #endregion
+    }
+}
---
+++ IIO11300Vktehtavat/Tehtava2/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tehtava2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtResults.Text = "";
+        }
+
+        private void btnDraw_Click(object sender, RoutedEventArgs e)
+        {
+            Lotto lotto = new Lotto();
+            int draws = int.Parse(txtDraws.Text);
+            int[] numbers;
+
+            try
+            {
+                if (cboGames.Text == "Suomi")
+                {
+                    for (int i = 0; i < draws; i++)
+                    {
+                        numbers = new int[7];
+                        numbers = lotto.DrawLotto();
+                        foreach (int j in numbers)
+                        {
+                            txtResults.Text += j + " ";
+                        }
+                        txtResults.Text += "\r\n";
+                    }
+                }
+                else if (cboGames.Text == "Viking Lotto")
+                {
+                    for (int i = 0; i < draws; i++)
+                    {
+                        numbers = new int[6];
+                        numbers = lotto.DrawVikingLotto();
+                        foreach (int j in numbers)
+                        {
+                            txtResults.Text += j + " ";
+                        }
+                        txtResults.Text += "\r\n";
+                    }
+                }
+                else if (cboGames.Text == "Eurojackpot")
+                {
+                    for (int i = 0; i < draws; i++)
+                    {
+                        numbers = new int[5];
+                        numbers = lotto.DrawEurojackpotMain();
+                        foreach (int j in numbers)
+                        {
+                            txtResults.Text += j + " ";
+                        }
+                        txtResults.Text += "+ ";
+                        numbers = new int[2];
+                        numbers = lotto.DrawEurojackpotStar();
+                        foreach (int j in numbers)
+                        {
+                            txtResults.Text += j + " ";
+                        }
+                        txtResults.Text += "\r\n";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                
+            }
+        }
+    }
+}
