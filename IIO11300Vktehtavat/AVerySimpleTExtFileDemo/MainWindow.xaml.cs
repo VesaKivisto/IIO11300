03ee095c255a694e743f17c1c64460f810b747f2
Vesa Kivistö <kivisto.vesku@gmail.com>
2016-02-02 17:26:21 +0200

    Tehtävä 3C tehty

---
+++ IIO11300Vktehtavat/AVerySimpleTExtFileDemo/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿/*
+* Copyright JAMK/IT/ETS
+* This file is part of the course IIO11300 Windows-ohjelmointi
+* Created: 28.1.2016 
+* Authors:Esa Salmikangas 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AVerySimpleTextFileDemo
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void btnWrite_Click(object sender, RoutedEventArgs e)
+    {
+      try
+      {
+        //kirjoitetaan annettu teksti tiedostoon riittävän monta kertaa
+        string filename = txtFileName.Text;
+        int count = 1;
+        int.TryParse(txtCount.Text, out count);
+        if (filename.Length > 0)
+        {
+          using (StreamWriter sw = File.CreateText(filename))
+          {
+            for (int i = 1; i <= count; i++)
+            {
+              sw.WriteLine(i.ToString() + ":" + txtText.Text);
+            }
+          }
+          tbMessages.Text = String.Format("Kirjoitettu {0} riviä tiedostoon {1}", count, filename);
+        }
+      }
+      catch (Exception ex)
+      {
+
+        tbMessages.Text = ex.Message;
+      }
+    }
+
+    private void btnReadFile_Click(object sender, RoutedEventArgs e)
+    {
+      txtResult.Text = null;
+      //luetaan teksitiedostoa rivi kerrallaan
+      string filename = txtFileName.Text;
+      string line = null;
+      if (filename.Length > 0)
+      {
+        using (StreamReader sr = File.OpenText(filename))
+        {
+          line = null;
+          do
+          {
+            line = sr.ReadLine();
+            txtResult.Text += line + "\n";
+          } while (line != null);
+        }
+      }
+    }
+  }
+}
---
+++ IIO11300Vktehtavat/Tehtava3/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace Tehtava3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHae_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string dirName = txtHakemisto.Text;
+                string[] fileNames = Directory.GetFiles(dirName, "*.txt");
+                lstTiedostot.Items.Clear();
+                foreach (string file in fileNames)
+                {
+                    lstTiedostot.Items.Add(file);
+                }
+                txtTulos.Text = ConfigurationManager.AppSettings["tulos"];
+            }
+            catch (Exception ex)
+            {
+                lblError.Content = ex.Message;
+            }
+        }
+
+        private void btnYhdista_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string dirName = txtHakemisto.Text;
+                string tulos = txtTulos.Text;
+                string[] fileNames = Directory.GetFiles(dirName);
+                string text = "";
+
+                foreach (string file in fileNames)
+                {
+                    text += File.ReadAllText(file) + "\r\n";
+                }
+
+                File.WriteAllText(tulos, text);
+            }
+            catch (Exception ex)
+            {
+                lblError.Content = ex.Message;
+            }
+        }
+    }
+}
