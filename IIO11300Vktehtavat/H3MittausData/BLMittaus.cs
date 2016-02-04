f8862a13936d1526696870237038c77449b76c11
VesaKivisto <kivisto.vesku@gmail.com>
2016-02-04 10:58:57 +0200

    Mittausdatan käsittely loppuun

--- IIO11300Vktehtavat/H3MittausData/BLMittaus.cs
+++ IIO11300Vktehtavat/H3MittausData/BLMittaus.cs
@@ -1,44 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JAMK.IT.IIO11300
 {
-  public class MittausData
-  {
-    private string kello;
-    public string Kello
+    [Serializable]
+    public class MittausData
     {
-      get { return kello; }
-      set { kello = value; }
-    }
-    private string mittaus;
+        #region PROPERTIES
+        private string kello;
+        public string Kello
+        {
+            get { return kello; }
+            set { kello = value; }
+        }
+        private string mittaus;
 
-    public string Mittaus
-    {
-      get { return mittaus; }
-      set { mittaus = value; }
+        public string Mittaus
+        {
+            get { return mittaus; }
+            set { mittaus = value; }
+        }
+        #endregion
+        #region CONSTRUCTORS
+        // Luokalle tehdään kaksi konstruktoria
+        public MittausData()
+        {
+            kello = "0:00";
+            mittaus = "empty";
+        }
+        public MittausData(string klo, string mdata)
+        {
+            this.kello = klo;
+            this.mittaus = mdata;
+        }
+        #endregion
+        #region METHODS
+        // Ylikirjoitetaan ToString
+        public override string ToString()
+        {
+            // Return base.ToString();
+            return kello + " = " + mittaus;
+        }
+        // Tiedoston käsittelymetodit
+        public static void SaveToFile(string filename, List<MittausData> mittaukset)
+        {
+            try
+            {
+                StreamWriter sw;
+                // Tarkistetaan onko tiedosto jo olemassa
+                if (File.Exists(filename))
+                {
+                    // Liitetään olemassa olevaan tiedostoon
+                    sw = File.AppendText(filename);
+                }
+                else
+                {
+                    // Luodaan uusi tiedosto
+                    sw = File.CreateText(filename);
+                }
+                // Kirjoitus
+                foreach (var item in mittaukset)
+                {
+                    sw.WriteLine(item);
+                }
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static void SaveToFileV2(string filename, List<MittausData> mittaukset)
+        {
+            try
+            {
+                // Luodaan uusi tai kirjoitetaan olemassa olevaan
+                StreamWriter sw = File.AppendText(filename);
+
+                // Kirjoitus
+                foreach (var item in mittaukset)
+                {
+                    sw.WriteLine(item);
+                }
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static List<MittausData> ReadFromFile(string filename)
+        {
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    // Luetaan tekstitiedosto ja muutetaan se MittausData-olioksi
+                    MittausData md;
+                    List<MittausData> luetut = new List<MittausData>();
+                    string rivi = "";
+                    StreamReader sr = File.OpenText(filename);
+                    while ((rivi = sr.ReadLine()) != null)
+                    {
+                        if (rivi.Length > 3 && rivi.Contains("="))
+                        {
+                            string[] split = rivi.Split(new char[] { '=' });
+                            // Alimerkkijonoista luodaan olio
+                            md = new MittausData(split[0], split[1]);
+                            luetut.Add(md);
+                        } 
+                    }
+                    sr.Close();
+                    // Palautetaan
+                    return luetut;
+                }
+                else
+                {
+                    throw new FileNotFoundException();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
     }
-    #region CONSTRUCTORS
-    //luokalle tehdään kaksi konstruktoria
-    public MittausData()
-    {
-      kello = "0:00";
-      mittaus = "empty";
-    }
-    public MittausData(string klo, string mdata)
-    {
-      this.kello = klo;
-      this.mittaus = mdata;
-    }
-    #endregion
-    //ylikirjoitetaan ToString
-    public override string ToString()
-    {
-      //return base.ToString();
-      return kello + "=" + mittaus;
-    }
-  }
 }
--- IIO11300Vktehtavat/H3MittausData/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/H3MittausData/MainWindow.xaml.cs
@@ -16,27 +16,121 @@ using JAMK.IT.IIO11300;
 
 namespace H3MittausData
 {
-  /// <summary>
-  /// Interaction logic for MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    public MainWindow()
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
-      InitMyStuff();
-    }
-    private void InitMyStuff()
-    {
-      //omat ikkunaan liittyvät alustukset
-      txtToday.Text = DateTime.Today.ToShortDateString();
-    }
+        // Luodaan lista mittausolioita varten
+        List<MittausData> mittaukset;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitMyStuff();
+        }
+        private void InitMyStuff()
+        {
+            //omat ikkunaan liittyvät alustukset
+            txtToday.Text = DateTime.Today.ToShortDateString();
+            mittaukset = new List<MittausData>();
+        }
 
-    private void btnSaveData_Click(object sender, RoutedEventArgs e)
-    {
-      //luodaan uusi mittausdata olio ja näytetään se käyttäjälle
-      MittausData md = new MittausData(txtClock.Text, txtData.Text);
-      lbData.Items.Add(md);
+        private void btnSaveData_Click(object sender, RoutedEventArgs e)
+        {
+            //luodaan uusi mittausdata olio ja näytetään se käyttäjälle
+            MittausData md = new MittausData(txtClock.Text, txtData.Text);
+            //lbData.Items.Add(md); //testausta varten
+            mittaukset.Add(md);
+            ApplyChanges();
+        }
+        private void ApplyChanges()
+        {
+            lbData.ItemsSource = null;
+            lbData.ItemsSource = mittaukset;
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Kirjoitetaan mitatut tiedostoon
+            try
+            {
+                MittausData.SaveToFileV2(txtFileName.Text, mittaukset);
+                MessageBox.Show("Tiedot tallennettu onnistuneesti tiedostoon " + txtFileName.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnRead_Click(object sender, RoutedEventArgs e)
+        {
+            // Haetaan käyttäjän antamasta tiedostosta mitatut arvot
+            try
+            {
+                mittaukset = MittausData.ReadFromFile(txtFileName.Text);
+                ApplyChanges();
+                MessageBox.Show("Tiedot haettu onnistuneesti tiedostosta " + txtFileName.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnSerialize_Click(object sender, RoutedEventArgs e)
+        {
+            // Kutsutaan serialisointia
+            try
+            {
+                Serialisointi.SerialisoiXml(txtFileName.Text, mittaukset);
+                MessageBox.Show("Serialisoitu onnistuneesti tiedostoon " + txtFileName.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnDeserialize_Click(object sender, RoutedEventArgs e)
+        {
+            // Kutsutaan deserialisointia
+            try
+            {
+                mittaukset = Serialisointi.DeSerialisoiXml(txtFileName.Text);
+                ApplyChanges();
+                MessageBox.Show("Deserialisoitu onnistuneesti tiedostosta " + txtFileName.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnSerializeBin_Click(object sender, RoutedEventArgs e)
+        {
+            // Kutsutaan serialisointia binäärimuotoon
+            try
+            {
+                Serialisointi.Serialisoi(txtFileName.Text, mittaukset);
+                MessageBox.Show("Binäärimuotoinen serialisointi onnistui tiedostoon " + txtFileName.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnDeserializeBin_Click(object sender, RoutedEventArgs e)
+        {
+            // Kutsutaan deserialisointia
+            object obj = new object();
+            try
+            {
+                Serialisointi.DeSerialisoi(txtFileName.Text, ref obj);
+                // Ja nyt koetaan astata object-tyyppinen olio listaksi mittausdataan
+                mittaukset = (List<MittausData>)obj;
+                ApplyChanges();
+                MessageBox.Show("Binäärimuotoinen deserialisointi onnistui tiedostosta " + txtFileName.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
-  }
 }
