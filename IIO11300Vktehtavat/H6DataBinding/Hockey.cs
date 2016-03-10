19b816f80ef56da1e87f0245863696778163ad70
VesaKivisto <kivisto.vesku@gmail.com>
2016-03-10 10:53:55 +0200

    H6DataBinding viimeistelty ja H7ADONETConsole tehty

--- IIO11300Vktehtavat/H6DataBinding/Hockey.cs
+++ IIO11300Vktehtavat/H6DataBinding/Hockey.cs
@@ -1,14 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace H6DataBinding
 {
-    public class HockeyPlayer
+    public static class TestHockeyBench
     {
+        public static List<HockeyPlayer> Get3Players()
+        {
+            List<HockeyPlayer> players = new List<HockeyPlayer>();
+            players.Add(new HockeyPlayer("Teemu Selänne", "8"));
+            players.Add(new HockeyPlayer("Jarkko Immonen", "26"));
+            players.Add(new HockeyPlayer("Ville Peltonen", "16"));
+            return players;
+        }
+    }
 
+    public class HockeyPlayer : INotifyPropertyChanged
+    {
+        // Properties
+        private string name;
+        public string Name
+        {
+            set
+            {
+                name = value;
+                Notify("Name");
+                Notify("NameAndNumber");
+            }
+            get
+            {
+                return name;
+            }
+        }
+
+        private string number;
+        public string Number
+        {
+            set
+            {
+                number = value;
+                Notify("Number");
+                Notify("NameAndNumber");
+            }
+            get
+            {
+                return number;
+            }
+        }
+
+        public string NameAndNumber
+        {
+            get
+            {
+                return name + " #" + number;
+            }
+        }
+
+        //Constructors
+        public HockeyPlayer()
+        {
+            
+        }
+        public HockeyPlayer(string name, string number)
+        {
+            this.name = name;
+            this.number = number;
+        }
+
+        // Rajapinnan (interface) toteutus
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
     }
 
     public class HockeyTeam
---
+++ IIO11300Vktehtavat/H7ADONETConsole/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace H7ADONETConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // 1 Luodaan yhteys
+                string connStr = H7ADONETConsole.Properties.Settings.Default.Tietokanta;
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    // Avataan yhteys
+                    conn.Open();
+                    // 2 Luodaan komento ja suoritetaan se
+                    SqlCommand cmd = new SqlCommand("SELECT asioid, lastname, firstname, date FROM presences WHERE asioid = 'H9073'", conn);
+                    // 3 Käydään tulos = Reader-olio läpi
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    // Käydään rdr läpi
+                    if (rdr.HasRows)
+                    {
+                        int lkm = 0;
+                        while (rdr.Read())
+                        {
+                            // Tulos näkyviin
+                            Console.WriteLine("Läsnäolosi {0} {1} {2} {3}", rdr.GetString(0), rdr.GetString(1), rdr.GetString(2), rdr.GetDateTime(3).ToShortDateString());
+                            lkm++;
+                        }
+                        Console.WriteLine("Tulostettu {0} läsnäoloa", lkm);
+                    }
+                    // Suljetaan yhteys
+                    rdr.Close();
+                    conn.Close();
+                    Console.WriteLine("Tietokantayhteys suljettu onnistuneesti!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
