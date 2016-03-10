44591d7d2feed7e6a2ae0afce13c3e5193a92288
Vesa Kivistö <kivisto.vesku@gmail.com>
2016-03-11 00:36:35 +0200

    Tehtävä 8 valmis

---
+++ IIO11300Vktehtavat/Tehtava8/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Tehtava8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetCustomers_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Tehtava8.Properties.Settings.Default.Tietokanta))
+                {
+                    string sql = "SELECT firstname, lastname, address, city FROM vCustomers";
+                    SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+                    DataTable dt = new DataTable("Asiakkaat");
+                    da.Fill(dt);
+                    lbCustomers.DataContext = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void lbCustomers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            spRight.DataContext = e.AddedItems[0];
+        }
+    }
+}
