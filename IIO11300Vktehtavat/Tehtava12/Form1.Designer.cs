2ed6feff8c0a4e6ee1b44acf931257f51e555ae3
Vesa Kivistö <kivisto.vesku@gmail.com>
2016-04-19 16:28:28 +0300

    Tehtävä 12C

---
+++ IIO11300Vktehtavat/Tehtava12/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Tehtava12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnUusi = new System.Windows.Forms.Button();
+            this.btnPeruuta = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cboLanguages = new System.Windows.Forms.ComboBox();
+            this.lblTunnus = new System.Windows.Forms.Label();
+            this.lblSala = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.cboTypes = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            resources.ApplyResources(this.btnOK, "btnOK");
+            this.btnOK.Name = "btnOK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnUusi
+            // 
+            resources.ApplyResources(this.btnUusi, "btnUusi");
+            this.btnUusi.Name = "btnUusi";
+            this.btnUusi.UseVisualStyleBackColor = true;
+            // 
+            // btnPeruuta
+            // 
+            resources.ApplyResources(this.btnPeruuta, "btnPeruuta");
+            this.btnPeruuta.Name = "btnPeruuta";
+            this.btnPeruuta.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // cboLanguages
+            // 
+            this.cboLanguages.FormattingEnabled = true;
+            resources.ApplyResources(this.cboLanguages, "cboLanguages");
+            this.cboLanguages.Name = "cboLanguages";
+            this.cboLanguages.SelectedIndexChanged += new System.EventHandler(this.cboLanguages_SelectedIndexChanged);
+            // 
+            // lblTunnus
+            // 
+            resources.ApplyResources(this.lblTunnus, "lblTunnus");
+            this.lblTunnus.Name = "lblTunnus";
+            // 
+            // lblSala
+            // 
+            resources.ApplyResources(this.lblSala, "lblSala");
+            this.lblSala.Name = "lblSala";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // cboTypes
+            // 
+            this.cboTypes.FormattingEnabled = true;
+            resources.ApplyResources(this.cboTypes, "cboTypes");
+            this.cboTypes.Name = "cboTypes";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblSala);
+            this.Controls.Add(this.lblTunnus);
+            this.Controls.Add(this.cboTypes);
+            this.Controls.Add(this.cboLanguages);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnPeruuta);
+            this.Controls.Add(this.btnUusi);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnUusi;
+        private System.Windows.Forms.Button btnPeruuta;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cboLanguages;
+        private System.Windows.Forms.Label lblTunnus;
+        private System.Windows.Forms.Label lblSala;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox cboTypes;
+    }
+}
+
---
+++ IIO11300Vktehtavat/Tehtava12/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Threading;
+using System.Resources;
+
+namespace Tehtava12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitStuff();
+        }
+
+        public void InitStuff()
+        {
+            string[] languages = new string[2] { "Finnish", "English" };
+            foreach (string language in languages)
+            {
+                cboLanguages.Items.Add(language);
+            }
+            cboLanguages.SelectedIndex = 0;
+
+            string[] types = new string[3] { "XML", "SSL", "ASD" };
+            foreach (string type in types)
+            {
+                cboTypes.Items.Add(type);
+            }
+        }
+
+        private void cboLanguages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string language = cboLanguages.SelectedItem.ToString();
+            if (language == "Finnish")
+            {
+                ResourceManager LocRM = new ResourceManager("Tehtava12.WinFormStrings", typeof(Form1).Assembly);
+                lblTunnus.Text = LocRM.GetString("lblTunnus");
+                lblSala.Text = LocRM.GetString("lblSala");
+                btnPeruuta.Text = LocRM.GetString("btnPeruuta");
+                btnUusi.Text = LocRM.GetString("btnUusi");
+            }
+            else if (language == "English")
+            {
+                ResourceManager LocRM = new ResourceManager("Tehtava12.WinFormStrings-en-GB", typeof(Form1).Assembly);
+                lblTunnus.Text = LocRM.GetString("lblTunnus");
+                lblSala.Text = LocRM.GetString("lblSala");
+                btnPeruuta.Text = LocRM.GetString("btnPeruuta");
+                btnUusi.Text = LocRM.GetString("btnUusi");
+            }
+        }
+    }
+}
